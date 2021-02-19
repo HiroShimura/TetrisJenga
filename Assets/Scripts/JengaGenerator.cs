a5bc0b374530e0bf29f9b6c944cb7c7b821f015a
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-02-19 20:20:05 +0900

    Prefab作成、一段目を生成

--- Assets/Scripts/JengaGenerator.cs
+++ Assets/Scripts/JengaGenerator.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Jengagenerator : MonoBehaviour
-{
+public class JengaGenerator : MonoBehaviour {
+    public GameObject tMinoPrefab;
+    public GameObject iMinoPrefab;
+    public GameObject oMinoPrefab;
+    public GameObject sMinoPrefab;
+    public GameObject zMinoPrefab;
+    public GameObject jMinoPrefab;
+    public GameObject lMinoPrefab;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        GameObject iMino1_1;
+        GameObject lMino1_1;
+        GameObject sMino1_1;
+        GameObject lMino1_2;
+        iMino1_1 = Instantiate(iMinoPrefab);
+        iMino1_1.transform.position = new Vector3(1.5f, 1, 0);
+        iMino1_1.transform.Rotate(0, 90, 0);
+        lMino1_1 = Instantiate(lMinoPrefab);
+        lMino1_1.transform.position = new Vector3(-0.5f, 1, -1);
+        lMino1_1.transform.Rotate(0, 180, 0);
+        sMino1_1 = Instantiate(sMinoPrefab);
+        sMino1_1.transform.position = new Vector3(-0.5f, 1, 0);
+        sMino1_1.transform.Rotate(0, 270, 0);
+        lMino1_2 = Instantiate(lMinoPrefab);
+        lMino1_2.transform.position = new Vector3(-0.5f, 1, 1);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }

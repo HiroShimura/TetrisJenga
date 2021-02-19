a6e0868f75372eeab32b71e88f5c0cc63ba96f20
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-02-19 17:55:08 +0900

    ミノを追加

---
+++ Assets/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// GameビューにてSceneビューのようなカメラの動きをマウス操作によって実現する
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour {
+    [SerializeField, Range(0.1f, 10f)]
+    private float wheelSpeed = 1f;
+
+    [SerializeField, Range(0.1f, 10f)]
+    private float moveSpeed = 0.3f;
+
+    [SerializeField, Range(0.1f, 10f)]
+    private float rotateSpeed = 0.3f;
+
+    private Vector3 preMousePos;
+
+    // public GameObject focusObject;
+
+    private void Update() {
+        MouseUpdate();
+    }
+
+    private void MouseUpdate() {
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollWheel != 0.0f) {
+            MouseWheel(scrollWheel);
+        }
+        if (Input.GetMouseButtonDown(0) ||
+           Input.GetMouseButtonDown(1) ||
+           Input.GetMouseButtonDown(2)) {
+            preMousePos = Input.mousePosition;
+        }
+        MouseDrag(Input.mousePosition);
+    }
+
+    private void MouseWheel(float delta) {
+        transform.position += transform.forward * delta * wheelSpeed;
+        return;
+    }
+
+    private void MouseDrag(Vector3 mousePos) {
+        Vector3 diff = mousePos - preMousePos;
+        if (diff.magnitude < Vector3.kEpsilon) {
+            return;
+        }
+        if (Input.GetMouseButton(2)) {
+            transform.Translate(-diff * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetMouseButton(1)) {
+            CameraRotate(new Vector2(-diff.y, diff.x) * rotateSpeed);
+        }
+        preMousePos = mousePos;
+    }
+
+    public void CameraRotate(Vector2 angle) {
+        Vector3 focusObjectPos = new Vector3(0, 0, 0);
+        transform.RotateAround(focusObjectPos, transform.right, angle.x);
+        transform.RotateAround(focusObjectPos, Vector3.up, angle.y);
+    }
+}
---
+++ Assets/MinoController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MinoController : MonoBehaviour {
+    Ray ray;
+    RaycastHit hit;
+    bool beRay = false;
+    GameObject target;
+    Vector3 targetPos;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RayCheck();
+            PositionCheck();
+        }
+        if (beRay) {
+            MovePosition();
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            beRay = false;
+        }
+    }
+
+    void RayCheck() {
+        if (Physics.Raycast(ray, out hit)) {
+            target = hit.collider.gameObject;
+            beRay = true;
+        }
+        /*
+        if (Physics.Raycast(ray, out hit) && hit.collider == gameObject.GetComponent<Collider>()) {
+            beRay = true;
+        }
+        */
+        else {
+            beRay = false;
+        }
+    }
+
+    void PositionCheck() {
+        Vector3 mousePos = Input.mousePosition;
+        float depth = Camera.main.transform.InverseTransformPoint(hit.point).z;
+        mousePos.z = depth;
+        targetPos = target.transform.position;
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        offset = targetPos - worldMousePos;
+    }
+
+    void MovePosition() {
+        Vector3 mousePos = Input.mousePosition;
+        float depth = Camera.main.transform.InverseTransformPoint(hit.point).z;
+        mousePos.z = depth;
+        Vector3 moveTo = Camera.main.ScreenToWorldPoint(mousePos);
+        target.transform.position = new Vector3(moveTo.x + offset.x, targetPos.y, moveTo.z + offset.z);
+    }
+}

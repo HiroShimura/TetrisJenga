9ebb406fc2637ec3c06ddca1f9ca4c4eefc790bb
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-02-19 20:32:51 +0900

    型別メソッド作成中

--- Assets/Scripts/JengaGenerator.cs
+++ Assets/Scripts/JengaGenerator.cs
@@ -10,21 +10,25 @@ public class JengaGenerator : MonoBehaviour {
     public GameObject lMinoPrefab;
     // Start is called before the first frame update
     void Start() {
-        GameObject iMino1_1;
-        GameObject lMino1_1;
-        GameObject sMino1_1;
-        GameObject lMino1_2;
-        iMino1_1 = Instantiate(iMinoPrefab);
+
+    }
+
+    void Type1(Vector3 rotatePos, float rotate) {
+        GameObject iMino1_1 = Instantiate(iMinoPrefab);
         iMino1_1.transform.position = new Vector3(1.5f, 1, 0);
-        iMino1_1.transform.Rotate(0, 90, 0);
-        lMino1_1 = Instantiate(lMinoPrefab);
+        iMino1_1.transform.Rotate(0, 90 + rotate, 0);
+
+        GameObject lMino1_1 = Instantiate(lMinoPrefab);
         lMino1_1.transform.position = new Vector3(-0.5f, 1, -1);
-        lMino1_1.transform.Rotate(0, 180, 0);
-        sMino1_1 = Instantiate(sMinoPrefab);
+        lMino1_1.transform.Rotate(0, 180 + rotate, 0);
+
+        GameObject sMino1_1 = Instantiate(sMinoPrefab);
         sMino1_1.transform.position = new Vector3(-0.5f, 1, 0);
-        sMino1_1.transform.Rotate(0, 270, 0);
-        lMino1_2 = Instantiate(lMinoPrefab);
+        sMino1_1.transform.Rotate(0, 270 + rotate, 0);
+
+        GameObject lMino1_2 = Instantiate(lMinoPrefab);
         lMino1_2.transform.position = new Vector3(-0.5f, 1, 1);
+        lMino1_2.transform.Rotate(0, rotate, 0);
     }
 
     // Update is called once per frame

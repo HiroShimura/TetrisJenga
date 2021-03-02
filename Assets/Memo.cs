962be67da48a5a99311fe13be5a42612c1b2abe1
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-02 17:58:06 +0900

    Update Memo.cs

--- Assets/Memo.cs
+++ Assets/Memo.cs
@@ -3,10 +3,13 @@
 public class Memo : MonoBehaviour {
 
     /*
-     * ミノを積んだ後の次のプレイヤーへの入れ替えにはコルーチンを使う
-     * Rayを使ったミノの高さを調べる方法を用意し、ミノを積む際のカメラ移動とペナルティーで利用
-     * ミノが落ちるかTableに触れたらGameOver
-     * Tableに触れた場合はコルーチンを使ってGameOver（できればTETRIS99みたいなミノが灰色になるアニメーションが欲しいけど...）
+     * ミノを積んだ後の次のプレイヤーへの入れ替えにはコルーチンを使う（3～4秒くらい？）
+     * 
+     * ミノの高さを調べる方法を用意し、ミノを積む際のカメラ移動とペナルティーで利用
+     * 
+     * ミノがTableの外側に落ちるかTableに触れたらGameOver
+     * 下のミノからRigidbodyを無効化&灰色マテリアルを適用（できれば下方向からのグラデーション付き）でTETRIS99みたいなミノが灰色になるアニメーションを再現可能か？
+     * GameOverの時点でCameraControllerやMinoCatcherを無効化するのを忘れずに
      */
 
     // Start is called before the first frame update

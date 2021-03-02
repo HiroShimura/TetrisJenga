d0d0f7d374107fbbb7654f36223a87ac3a50d2a1
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-02 15:50:01 +0900

    Update Memo.cs

--- Assets/Memo.cs
+++ Assets/Memo.cs
@@ -3,10 +3,11 @@
 public class Memo : MonoBehaviour {
 
     /*
-     *ミノを積んだ後の次のプレイヤーへの入れ替えにはコルーチンを使う
-     *Rayを使ったミノの高さを調べる方法を用意し、ミノを積む際のカメラ移動とペナルティーで利用
-     * 
-    */
+     * ミノを積んだ後の次のプレイヤーへの入れ替えにはコルーチンを使う
+     * Rayを使ったミノの高さを調べる方法を用意し、ミノを積む際のカメラ移動とペナルティーで利用
+     * ミノが落ちるかTableに触れたらGameOver
+     * Tableに触れた場合はコルーチンを使ってGameOver（できればTETRIS99みたいなミノが灰色になるアニメーションが欲しいけど...）
+     */
 
     // Start is called before the first frame update
     void Start() {

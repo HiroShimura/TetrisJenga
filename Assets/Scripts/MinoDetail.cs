49923af3f37812afd92dbc619da22b0c7b1ad849
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-03 18:22:17 +0900

    ミノにカーソルを置くとログが出るように変更
    カーソルを置いたら光らせるためのテスト

---
+++ Assets/Scripts/MinoDetail.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MinoDetail : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnMouseEnter() {
+        Debug.Log("マウスオーバー");
+    }
+    private void OnMouseExit() {
+        Debug.Log("カーソルが外れました");
+    }
+}

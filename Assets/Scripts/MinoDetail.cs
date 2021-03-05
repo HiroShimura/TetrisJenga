4a117229cdf0c156d8f41728417ce2c3eadf164d
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-05 19:54:15 +0900

    ミノの色が変わる条件式を修正

--- Assets/Scripts/MinoDetail.cs
+++ Assets/Scripts/MinoDetail.cs
@@ -14,18 +14,14 @@ public class MinoDetail : MonoBehaviour {
     Color color;
     private void OnMouseEnter() {
         color = GetComponent<Renderer>().material.color;
-        if ((GameObject.FindWithTag("SelectedMino") == null || (GameObject.FindWithTag("StackedMino") != null)) && Cursor.visible == true) {
+        if ((GameObject.FindWithTag("SelectedMino") == null || CompareTag("StackedMino")) && Cursor.visible == true) {
             GetComponent<Renderer>().material.color = Color.white;
         }
     }
 
     private void OnMouseExit() {
-        if ((GameObject.FindWithTag("SelectedMino") == null || (GameObject.FindWithTag("StackedMino") != null)) && Cursor.visible == true) {
+        if (GetComponent<Renderer>().material.color == Color.white) {
             GetComponent<Renderer>().material.color = color;
-        }
-        else if (GetComponent<Renderer>().material.color == Color.white) {
-            GetComponent<Renderer>().material.color = color;
-
         }
     }
 }

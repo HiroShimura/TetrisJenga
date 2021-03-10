ca8d11711fc432790be3afd33f279d07f99327e5
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-10 22:00:35 +0900

    仮のタイトル画面を実装、メモをtxt形式に変更

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class StartButton : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}

6cb9beb3ed7fe27a208947c3ca158c4c01fd2383
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-13 16:16:11 +0900

    UI調整

--- Assets/Scripts/Clockwise.cs
+++ Assets/Scripts/Clockwise.cs
@@ -8,6 +8,6 @@ public class Clockwise : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.Rotate(0, 0, -0.3f);
+        transform.Rotate(0, 0, -0.1f);
     }
 }

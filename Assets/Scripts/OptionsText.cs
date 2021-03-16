9cc6f3fd845f7df1bf860d5a3bfb31d4a7ca981b
HiroShimura <74451547+HiroShimura@users.noreply.github.com>
2021-03-16 18:20:10 +0900

    OptionではSingletonやめた

--- Assets/Scripts/OptionsText.cs
+++ Assets/Scripts/OptionsText.cs
@@ -1,21 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-/*
-class Options {
-    static Options option;
-    public float Layers { get; set; } = GameObject.Find($"LayerSlider").GetComponent<Slider>().value;
-    public float Time { get; set; } = GameObject.Find($"TimeSlider").GetComponent<Slider>().value;
-    Options() {
-    }
-    public static Options Option() {
-        if (option == null) {
-            option = new Options();
-        }
-        return option;
-    }
-}
-*/
 public class OptionsText : MonoBehaviour {
 
     GameObject slider;
@@ -23,8 +8,6 @@ public class OptionsText : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        // float layers = Options.Option().Layers;
-        // float time = Options.Option().Time;
         slider = GameObject.Find($"{name}Slider");
         text = GetComponent<Text>().text;
         slider.GetComponent<Slider>().value = PlayerPrefs.GetInt(name);
@@ -38,10 +21,12 @@ public class OptionsText : MonoBehaviour {
 
     public void ValueChanged() {
         if (name == "Time") {
+            // time = GetComponent<Slider>().value;
             GetComponent<Text>().text = $"{text}{slider.GetComponent<Slider>().value}秒";
             PlayerPrefs.SetInt("Time", (int)slider.GetComponent<Slider>().value);
         }
         else {
+            // layers = GetComponent<Slider>().value;
             GetComponent<Text>().text = $"{text}{slider.GetComponent<Slider>().value}";
             PlayerPrefs.SetInt("Layer", (int)slider.GetComponent<Slider>().value);
         }
